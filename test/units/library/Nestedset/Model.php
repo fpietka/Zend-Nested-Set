9e8e01ee19b53c32fafe61feebb38f3d9d8984c6
François Pietka <francois@pietka.fr>
2014-07-24 21:08:19 +0200

    initialize tests with setters

---
+++ test/units/library/Nestedset/Model.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Units;
+
+// Add library to include path (where ZF should also be located)
+set_include_path(__DIR__ . '/../../../../library');
+
+require_once 'Nestedset/Model.php';
+
+// Include needed ZendFramework classes
+require_once 'Zend/Db.php';
+require_once 'Zend/Db/Table.php';
+require_once 'Zend/Db/Adapter/Abstract.php';
+
+$options = array('dbname' => 'test');
+$db = \Zend_Db::factory('PDO_SQLITE', $options);
+
+use mageekguy\atoum;
+
+class NestedSet_Model extends atoum\test
+{
+    public function testSetTableName()
+    {
+        $nestedset = new \NestedSet_Model();
+
+        $nestedset->setTableName('foo');
+        $this->assert->string($nestedset->getTableName())
+            ->isEqualTo('foo');
+    }
+
+    public function testSetDb()
+    {
+        $nestedset = new \NestedSet_Model();
+
+        $db = \Zend_Db::factory('Pdo_Sqlite', array('dbname' => 'dbtest'));
+        $nestedset->setDb($db);
+        $this->assert->object($nestedset->getDb())
+            ->isInstanceOf('\Zend_Db_Adapter_Abstract');
+    }
+
+    public function testSetStructureId()
+    {
+        $nestedset = new \NestedSet_Model();
+
+        $nestedset->setStructureId('foo');
+        $this->assert->string($nestedset->getStructureId())
+            ->isEqualTo('foo');
+    }
+
+    public function testSetStructureName()
+    {
+        $nestedset = new \NestedSet_Model();
+
+        $nestedset->setStructureName('foo');
+        $this->assert->string($nestedset->getStructureName())
+            ->isEqualTo('foo');
+    }
+
+    public function testSetStructureLeft()
+    {
+        $nestedset = new \NestedSet_Model();
+
+        $nestedset->setStructureLeft('foo');
+        $this->assert->string($nestedset->getStructureLeft())
+            ->isEqualTo('foo');
+    }
+
+    public function testSetStructureRight()
+    {
+        $nestedset = new \NestedSet_Model();
+
+        $nestedset->setStructureRight('foo');
+        $this->assert->string($nestedset->getStructureRight())
+            ->isEqualTo('foo');
+    }
+}
