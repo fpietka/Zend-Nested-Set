e50657bcb894d90d6c9e9bba2776c0e256e9918a
François Pietka <francois@pietka.fr>
2014-08-24 19:20:06 +0200

    add clover log file writing to atoum

--- .atoum.php
+++ .atoum.php
@@ -4,3 +4,11 @@ use mageekguy\atoum;
 
 // because of SQLite, we avoid concurrency
 $runner->setMaxChildrenNumber(1);
+
+$script->addDefaultReport();
+
+$cloverWriter = new atoum\writers\file('./build/logs/clover.xml');
+$cloverReport = new atoum\reports\asynchronous\clover();
+$cloverReport->addWriter($cloverWriter);
+
+$runner->addReport($cloverReport);
