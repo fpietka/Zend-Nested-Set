043a5db8c0cc70f36359abcc73108adf07ae5e98
François Pietka <francois@pietka.fr>
2014-12-13 15:10:04 +0100

    add new builder class to test class

--- tests/unit/library/Nestedset/ModelTest.php
+++ tests/unit/library/Nestedset/ModelTest.php
@@ -4,6 +4,7 @@
 set_include_path(__DIR__ . '/../../../../library');
 
 require_once 'Nestedset/Model.php';
+require_once 'Nestedset/Model/Builder.php';
 
 // Include needed ZendFramework classes
 require_once 'Zend/Db.php';
